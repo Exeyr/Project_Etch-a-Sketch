6992cb57d77b95f187439fd6633c4ba7b4086c4c
Exeyr <peep.podder@gmail.com>
2022-10-10 16:45:47 +0300

    rewrote the generateGrid function so that it would call createLines which creates the users input amount of lines and then calls createItems, which creates the users input amount of individual elements in the lines that createItems created. Also added a check at the beginning of createLines that checks whether there already is a grid or not and if there is it deletes it before creating a new one

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const btn = document.createElement('button');
-    body.appendChild(btn);
-    btn.setAttribute('id', 'button');
-
 const container = document.querySelector('#container');
 
 const etchDiv = document.createElement('div');
     etchDiv.setAttribute('id', 'etchDiv');
 
-btn.onclick = generateGrid();
+const etchContainer = document.createElement('div');
+    etchContainer.setAttribute('id', 'etchContainer');
 
 function generateGrid() {
     let XandY = prompt("Please enter the size of the grid you want to create", "16");
@@ -17,12 +14,38 @@ function generateGrid() {
         alert("Please only enter numbers!");
     }
     
+    else if (XandYNum > 27) {
+        alert("Please keep the grid size to a maximum of 27");
+    }
+    
     else {
-        for (let i = 0; i < (XandYNum * XandYNum); i++){
-            cloneEtchDiv = etchDiv.cloneNode();
-            container.appendChild(cloneEtchDiv);
-            
-        }
+        createLines();
+    }
+}
 
+function createLines() {
+    if (document.getElementById('etchContainer') !== null) {
+        container.innerHTML = '';
+        for (let i = 0; i < (XandYNum); i++){
+            cloneEtchContainer = etchContainer.cloneNode();
+            container.appendChild(cloneEtchContainer);
+            createItems();
+            }
+        
+    }
+    
+    else {
+        for (let i = 0; i < (XandYNum); i++){
+        cloneEtchContainer = etchContainer.cloneNode();
+        container.appendChild(cloneEtchContainer);
+        createItems();
+        }
+    }
+}
+
+function createItems() {
+    for (let i = 0; i < XandYNum; i++) {
+        cloneEtchDiv = etchDiv.cloneNode();
+        cloneEtchContainer.appendChild(cloneEtchDiv);
     }
 }
