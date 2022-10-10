2e2e752d581055d7d225ceeddcafc9916f752dea
Exeyr <peep.podder@gmail.com>
2022-10-10 15:23:27 +0300

    Managed to make a function that asks the user for a number and then makes a grid of divs times 2 of that number. Now need to make it so that it squares it instead.

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn = document.createElement('button');
+    body.appendChild(btn);
+    btn.setAttribute('id', 'button');
+
+const container = document.querySelector('#container');
+
+const etchDiv = document.createElement('div');
+    etchDiv.setAttribute('id', 'etchDiv');
+
+btn.onclick = generateGrid();
+
+function generateGrid() {
+    let XandY = prompt("Please enter the size of the grid you want to create", "16");
+    XandYNum = parseInt(XandY);
+    
+    if (isNaN(XandYNum)) {
+        alert("Please only enter numbers!");
+    }
+    
+    else {
+        for (let i = 0; i <= (2 * XandYNum); i++){
+            cloneEtchDiv = etchDiv.cloneNode();
+            container.appendChild(cloneEtchDiv);
+            
+        }
+
+    }
+}
