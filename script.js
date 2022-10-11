f6b69c4a0692a8a32113d6398816698d2138622e
Exeyr <peep.podder@gmail.com>
2022-10-11 13:29:27 +0300

    Played around with grid layout and figured out how to fill it up in an even manner

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const container = document.querySelector('#container');
 
-const etchDiv = document.createElement('div');
-    etchDiv.setAttribute('id', 'etchDiv');
-      
-
-const etchContainer = document.createElement('div');
-    etchContainer.setAttribute('id', 'etchContainer');
-
 function generateGrid() {
     let XandY = prompt("Please enter the size of the grid you want to create", "16");
     XandYNum = parseInt(XandY);
@@ -15,41 +8,40 @@ function generateGrid() {
         alert("Please only enter numbers!");
     }
     
-    else if (XandYNum > 27) {
-        alert("Please keep the grid size to a maximum of 27");
+    else if (XandYNum > 100) {
+        alert("Please keep the grid size to a maximum of 100");
     }
     
     else {
-        createLines();
+        createGrid();
     }
 }
 
-function createLines() {
-    if (document.getElementById('etchContainer') !== null) {
+function createGrid() {
+    container.setAttribute('style', `grid-template-columns: repeat(${XandYNum}, 2fr); grid-template-rows: repeat(${XandYNum}, 2fr);`);
+    if (document.getElementById('container') !== null) {
         container.innerHTML = '';
-        for (let i = 0; i < (XandYNum); i++){
-            cloneEtchContainer = etchContainer.cloneNode();
-            container.appendChild(cloneEtchContainer);
-            createItems();
+        for (let i = 0; i < (XandYNum * XandYNum); i++){
+            const etchDiv = document.createElement('div');
+            etchDiv.classList.add('etchDiv');
+            etchDiv.addEventListener('mouseover', function(event){
+                event.target.style.backgroundColor = 'black';
+            
+        })
+        container.appendChild(etchDiv); 
+    }
+        } else {
+            for (let i = 0; i < (XandYNum * XandYNum); i++){
+                const etchDiv = document.createElement('div');
+                etchDiv.classList.add('etchDiv');
+                etchDiv.addEventListener('mouseover', function(event){
+                    event.target.style.backgroundColor = 'black';
+                
+            })
+            container.appendChild(etchDiv);
             }
         
     }
-    
-    else {
-        for (let i = 0; i < (XandYNum); i++){
-        cloneEtchContainer = etchContainer.cloneNode();
-        container.appendChild(cloneEtchContainer);
-        createItems();
-        }
-    }
 }
 
-function createItems() {
-    for (let i = 0; i < XandYNum; i++) {
-        cloneEtchDiv = etchDiv.cloneNode();
-        cloneEtchContainer.appendChild(cloneEtchDiv);
-        clonEtchDiv.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'black';
-        }); 
-    }
-}
+generateGrid();
